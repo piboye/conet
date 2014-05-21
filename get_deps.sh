a34a99770634a39c0f4db986b2e994d514a40356
root <root@TENCENT64.site>
2014-05-22 02:54:54 +0800

    use thirdparty

--- get_deps.sh
+++ get_deps.sh
@@ -1,23 +1,42 @@
-cd thirdparty/
+echo "get thirdparty"
+if [[ ! -d thirdparty ]]
+then
+git clone https://github.com/chen3feng/thirdparty thirdparty
+fi
+
+echo "get blade"
+which blade
+if (( $? != 0 ))
+then
+    git clone https://github.com/chen3feng/typhoon-blade.git blade/
+    (cd blade; ./install)
+fi
+
+exit
+
 
 echo "get gtest"
 if [[ ! -d gtest ]]
 then
     svn checkout http://googletest.googlecode.com/svn/trunk/ gtest
+    (cd gtest; cmake -f CMakeLists.txt)
 #else 
     #(cd gtest; svn update)
 fi
 cp GTEST_BUILD gtest/BUILD
+cp -fr gtest/include/gtest include/
 
 echo "get gmock"
 if [[ ! -d gmock ]]
 then
     svn checkout http://googlemock.googlecode.com/svn/trunk/ gmock
+    (cd gmock; cmake -f CMakeLists.txt; make)
 #else
     #(cd gmock; svn update)
 fi
-
 cp GMOCK_BUILD gmock/BUILD
+cp -fr gmock/include/gmock include/
+cp -fr gmock/gtest/include/gtest include/
 
 echo "get glog"
 if [[ ! -d glog ]]
@@ -27,13 +46,18 @@ then
 #else
     #(cd glog; svn update)
 fi
+cp GLOG_BUILD glog/BUILD
+cp -r glog/src/glog include/
 
 
-#echo "get gflags"
-#if [[ ! -d gflags ]]
-#then
-#    git clone https://code.google.com/p/gflags/
-#    (cd gflags; )
-##else
-#    #(cd glog; svn update)
-#fi
+echo "get gflags"
+if [[ ! -d gflags ]]
+then
+    git clone https://code.google.com/p/gflags/
+    (cd gflags; cmake -f CMakeLists.txt)
+#else
+    #(cd glog; svn update)
+fi
+cp GFLAGS_BUILD gflags/BUILD
+cp -r gflags/include/gflags include/
+
