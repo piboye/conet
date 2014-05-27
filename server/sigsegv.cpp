9e490cc0feb91d369fdbdc82b4ee9c7300a2fe75
piboye <piboye@qq.com>
2014-05-27 19:01:55 +0800

    change to libunwind

--- server/sigsegv.cpp
+++ server/sigsegv.cpp
@@ -16,6 +16,10 @@
  *
  * =====================================================================================
  */
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <memory.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -24,6 +28,9 @@
 #include <ucontext.h>
 #include <dlfcn.h>
 #include <cxxabi.h>
+#include <execinfo.h>
+#include <libunwind.h>
+#include <assert.h>
 
 using __cxxabiv1::__cxa_demangle;
 
@@ -52,7 +59,47 @@ static const char * g_regs_name[NGREG] =
 
 #define REGFORMAT "%016lx"
 
-void print_ucontext(ucontext *uc, int fd)
+void print_ucontext(ucontext_t *a_uc, int fd)
+{
+    unw_cursor_t cursor; 
+    unw_context_t *uc=(unw_context_t *)(a_uc); 
+    unw_word_t ip, sp; 
+    static char symname[10000];
+	static char cxx_name[102400];
+
+	static char outbuf[102400];
+	#define uc_out(x, ...)   \
+	do {\
+		int len = snprintf(outbuf, sizeof(outbuf)-1, x "\n", ##__VA_ARGS__);\
+		if (len > (int) sizeof(outbuf)) len = sizeof(outbuf); \
+		write(fd, outbuf, len);\
+	} while(0) \
+
+	for(int i = 0; i < (int) NGREG; ++i) {
+        uc_out("reg[%02d:%s]=\t0x" REGFORMAT, i, g_regs_name[i], uc->uc_mcontext.gregs[i]);
+	}
+    uc_out("Stack trace:");
+
+    int ret = unw_init_local(&cursor, uc);
+    do {
+        if (ret) break;
+
+        ret = unw_get_reg(&cursor, UNW_REG_IP, &ip);
+        ret = unw_get_reg(&cursor, UNW_REG_SP, &sp);
+        ret = unw_get_proc_name(&cursor, symname, sizeof(symname), NULL);
+        size_t name_len = sizeof(cxx_name);
+        int status = 0;
+        __cxa_demangle(symname, cxx_name, &name_len, &status);
+        char *pname = symname;
+        if (status == 0) 
+            pname = cxx_name;
+        uc_out("%s: ip: %lx, sp: %lx", pname, (long) ip, (long) sp);
+    } while (unw_step(&cursor) > 0);
+    uc_out("End of stack trace.");
+}
+
+/*
+void print_ucontext(ucontext_t *uc, int fd)
 {
 	static char outbuf[102400];
 	static char cxx_name[102400];
@@ -74,18 +121,23 @@ void print_ucontext(ucontext *uc, int fd)
 
     uc_out("Stack trace:");
 	
+    //static void *bt[100];
+	//int sz = backtrace(bt, 100);
+	//backtrace_symbols_fd(bt, sz, fd);
+    //addr2line
 	int f = 0;
     while(bp && ip) {
         if(!dladdr(ip, &dlinfo))
             break;
         const char *symname = dlinfo.dli_sname;
+        uc_out("%s,%s", dlinfo.dli_sname, dlinfo.dli_fname);
 		
         int status=0;
 		
         size_t name_len = sizeof(cxx_name);
         char * name = __cxa_demangle(symname, cxx_name, &name_len, &status);
 
-        if (status == 0 && name)
+        if (status == 0)
             symname = name;
 
         uc_out("%2d: %p <%s+%lu> (%s)", ++f, ip, 
@@ -95,10 +147,10 @@ void print_ucontext(ucontext *uc, int fd)
 				 
         ip = bp[1];
         bp = (void**)bp[0];
-    }
+    }  
     uc_out("End of stack trace.");
 }
-
+*/
 
 
 static 
@@ -122,7 +174,8 @@ void signal_handle(int signum, siginfo_t* info, void*ptr)
     sigsegv_outp("info.si_errno = %d", info->si_errno);
     sigsegv_outp("info.si_code  = %d (%s)", info->si_code, si_codes[info->si_code]);
     sigsegv_outp("info.si_addr  = %p", info->si_addr);
-	print_ucontext(ucontext, fd);
+	//print_ucontext(ucontext, fd);
+    print_ucontext(ucontext, fd);
     _exit (-1);
 }
 
@@ -132,7 +185,7 @@ static int s_catch_sig[] = {SIGILL, SIGBUS, SIGFPE, SIGABRT, SIGPIPE, SIGSEGV};
 
 static void __attribute__((constructor)) setup_sig() 
 {
-    stack_t sigstack;
+    static stack_t sigstack;
     sigstack.ss_sp = s_sig_stack;
     sigstack.ss_size = sizeof(s_sig_stack);
     if (sigaltstack(&sigstack, NULL) == -1) {
