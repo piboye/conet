57c7cb4991769cf970bbb4fd037eec94469e8838
piboye <piboye@qq.com>
2014-07-28 01:05:39 +0800

    add

--- svrkit/pb2json.cpp
+++ svrkit/pb2json.cpp
@@ -193,6 +193,149 @@ int parse_msg(const google::protobuf::Message *msg, Json::Value * a_root)
 	return 0; 
 }
 
+void json2pb(Json::Value *root, google::protobuf::Message* msg);
+
+void json2field(Json::Value *root, 
+        google::protobuf::Message *msg, 
+        const google::protobuf::FieldDescriptor *field)
+{
+	const Reflection *ref = msg->GetReflection();
+	const bool repeated = field->is_repeated();
+
+	switch (field->cpp_type())
+	{
+
+#define _SET_OR_ADD(sfunc, afunc, value)			\
+		do {						\
+			if (repeated)				\
+				ref->afunc(&msg, field, value);	\
+			else					\
+				ref->sfunc(&msg, field, value);	\
+		} while (0)
+
+#define _CONVERT(type, ctype, fmt, sfunc, afunc) 		\
+		case FieldDescriptor::type: {			\
+			ctype value;				\
+			int r = json_unpack_ex(jf, &error, JSON_STRICT, fmt, &value); \
+			if (r) throw j2pb_error(field, std::string("Failed to unpack: ") + error.text); \
+			_SET_OR_ADD(sfunc, afunc, value);	\
+			break;					\
+		}
+
+
+        case google::protobuf::FieldDescriptor::CPPTYPE_DOUBLE:
+            {
+                double value = root[
+
+            }
+            break;
+
+		_CONVERT(CPPTYPE_DOUBLE, double, "f", SetDouble, AddDouble);
+		_CONVERT(CPPTYPE_FLOAT, double, "f", SetFloat, AddFloat);
+		_CONVERT(CPPTYPE_INT64, json_int_t, "I", SetInt64, AddInt64);
+		_CONVERT(CPPTYPE_UINT64, json_int_t, "I", SetUInt64, AddUInt64);
+		_CONVERT(CPPTYPE_INT32, json_int_t, "I", SetInt32, AddInt32);
+		_CONVERT(CPPTYPE_UINT32, json_int_t, "I", SetUInt32, AddUInt32);
+		_CONVERT(CPPTYPE_BOOL, int, "b", SetBool, AddBool);
+
+		case FieldDescriptor::CPPTYPE_STRING: {
+			if (!json_is_string(jf))
+				throw j2pb_error(field, "Not a string");
+			const char * value = json_string_value(jf);
+			if(field->type() == FieldDescriptor::TYPE_BYTES)
+				_SET_OR_ADD(SetString, AddString, b64_decode(value));
+			else
+				_SET_OR_ADD(SetString, AddString, value);
+			break;
+		}
+		case FieldDescriptor::CPPTYPE_MESSAGE: {
+			Message *mf = (repeated)?
+				ref->AddMessage(&msg, field):
+				ref->MutableMessage(&msg, field);
+			_json2pb(*mf, jf);
+			break;
+		}
+		case FieldDescriptor::CPPTYPE_ENUM: {
+			const EnumDescriptor *ed = field->enum_type();
+			const EnumValueDescriptor *ev = 0;
+			if (json_is_integer(jf)) {
+				ev = ed->FindValueByNumber(json_integer_value(jf));
+			} else if (json_is_string(jf)) {
+				ev = ed->FindValueByName(json_string_value(jf));
+			} else
+				throw j2pb_error(field, "Not an integer or string");
+			if (!ev)
+				throw j2pb_error(field, "Enum value not found");
+			_SET_OR_ADD(SetEnum, AddEnum, ev);
+			break;
+		}
+		default:
+			break;
+	}
+}
+
+int json2pb(Json::Value const *root, google::protobuf::Message *msg)
+{
+	const Descriptor *d = msg->GetDescriptor();
+	const Reflection *ref = msg->GetReflection();
+	if (!d || !ref) return -1;
+
+	for (typeof(root->begin()) it = root->begin(), iend = root->end();
+            it != iend; ++it)
+	{
+		const char *name = it->memberName();
+
+		const FieldDescriptor *field = d->FindFieldByName(name);
+		if (!field)
+			field = ref->FindKnownExtensionByName(name);
+
+		if (!field) return -2;
+
+		int r = 0;
+		if (field->is_repeated()) {
+			if (!json_is_array(jf))
+				throw j2pb_error(field, "Not array");
+			for (size_t j = 0; j < json_array_size(jf); j++)
+				_json2field(msg, field, json_array_get(jf, j));
+		} else
+			_json2field(msg, field, jf);
+	}
+}
+
+void json2pb(Message &msg, const char *buf, size_t size)
+{
+	json_t *root;
+	json_error_t error;
+
+	root = json_loadb(buf, size, 0, &error);
+
+	if (!root)
+		throw j2pb_error(std::string("Load failed: ") + error.text);
+
+	json_autoptr _auto(root);
+
+	if (!json_is_object(root))
+		throw j2pb_error("Malformed JSON: not an object");
+
+	_json2pb(msg, root);
+}
+
+int json_dump_std_string(const char *buf, size_t size, void *data)
+{
+	std::string *s = (std::string *) data;
+	s->append(buf, size);
+	return 0;
+}
+
+std::string pb2json(const Message &msg)
+{
+	std::string r;
+
+	json_t *root = _pb2json(msg);
+	json_dump_callback(root, json_dump_std_string, &r, 0);
+	return r;
+}
+
 }
 
 namespace conet
