9606660ecae0cf1fa5abf62006042c67510cd83f
piboye <piboye@qq.com>
2014-10-11 20:12:14 +0800

    fix pb2qlite

--- base/incl/pb2sqlite.h
+++ base/incl/pb2sqlite.h
@@ -16,42 +16,61 @@
 namespace conet 
 {
 
-class Pb2Sqlite
-{
-public:
+    class Pb2Sqlite
+    {
+        public:
+            Pb2Sqlite()
+            {
+                m_db = NULL;
+                m_hold_db = 0;
+            }
 
-int init(std::string const &file_name, std::string const &table);
+            ~Pb2Sqlite()
+            {
+                if (m_hold_db) {
+                    sqlite3_close(m_db);
+                }
+            }
 
-int create_table(const char * sql);
+            int init(sqlite3 *db, std::string const &table)
+            {
+                m_db = db;
+                m_table_name = table;
+            }
 
-int insert(google::protobuf::Message& message);
+            int init(std::string const &file_name, std::string const &table);
 
-int update(google::protobuf::Message & wheres, google::protobuf::Message &updates);
+            int create_table(const char * sql);
 
-int update(uint64_t id,  google::protobuf::Message &updates);
+            int insert(google::protobuf::Message& message);
 
-int get(google::protobuf::Message & wheres, google::protobuf::Message *result);
+            int update(google::protobuf::Message & wheres, google::protobuf::Message &updates);
 
-int get_all(google::protobuf::Message & wheres, std::vector<google::protobuf::Message* > *results);
+            int update(uint64_t id,  google::protobuf::Message &updates);
 
-int get(uint64_t id, google::protobuf::Message *result);
+            int get(google::protobuf::Message & wheres, google::protobuf::Message *result);
 
-int remove(uint64_t id);
+            int get_all(google::protobuf::Message & wheres, std::vector<google::protobuf::Message* > *results);
 
-int replace(google::protobuf::Message const &updates);
+            int get(uint64_t id, google::protobuf::Message *result);
 
-int insert_or_update(google::protobuf::Message const &updates);
+            int remove(uint64_t id);
 
-std::string m_table_name;
+            int replace(google::protobuf::Message const &updates);
 
-sqlite3 *m_db;
+            int insert_or_update(google::protobuf::Message const &updates);
 
-};
+            std::string m_table_name;
+
+            sqlite3 *m_db;
+            int m_hold_db;
+
+    };
 
 
-int Pb2Map(
-    const google::protobuf::Message& message,
-    std::map<std::string, std::string> *out);
+    int Pb2Map(
+            const google::protobuf::Message& message,
+            std::map<std::string, std::string> *out);
 
 }
 
