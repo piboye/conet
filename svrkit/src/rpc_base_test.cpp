3e8db20f6dde3b7f22f03353dcfa2d6f122e27dc
piboye <piboye@qq.com>
2014-10-27 10:05:52 +0800

    add test

---
+++ svrkit/src/rpc_base_test.cpp
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  rpc_base_test.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年10月27日 09时56分30秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+#include "svrkit/rpc_base_pb.pb.h"
+#include "gtest/gtest.h"
+
+TEST(p, a)
+{
+    conet_rpc_pb::CmdBase a, b;
+    a.set_seq_id(123);
+    b.set_server_name("abc");
+    std::string s;
+    b.SerializeToString(&s);
+    a.ParseFromString(s);
+    ASSERT_EQ(0, (int)a.seq_id());
+    ASSERT_STREQ("abc", a.server_name().c_str());
+}
+
