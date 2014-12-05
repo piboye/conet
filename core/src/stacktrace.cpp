5bba2e767d928fbd5b39906df1a59364a27e400b
piboye <piboye@qq.com>
2014-12-06 07:19:36 +0800

    remove comment code

--- core/src/stacktrace.cpp
+++ core/src/stacktrace.cpp
@@ -30,8 +30,6 @@
 #include <sys/types.h>
 #include <dlfcn.h>
 #include <unwind.h>
-#include <libunwind.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -45,9 +43,6 @@ struct bt_frame {
 };
 
 
-
-//#include "libunwind/libunwind.h"
-
 using __cxxabiv1::__cxa_demangle;
 
 #define REGFORMAT "%016lx"
@@ -59,69 +54,9 @@ using __cxxabiv1::__cxa_demangle;
         write(fd, outbuf, len);\
     } while(0) \
 
-/*
-static const char * g_regs_name[NGREG] =
-{
-    "GS",
-    "FS",
-    "ES",
-    "DS",
-    "EDI",
-    "ESI",
-    "EBP",
-    "ESP",
-    "EBX",
-    "EDX",
-    "ECX",
-    "EAX",
-    "TRAPNO",
-    "ERR",
-    "EIP",
-    "CS",
-    "EFL",
-    "UESP",
-    "SS"
-};
-*/
-
 
 namespace conet
 {
-/*
-void print_ucontext(ucontext_t *a_uc, int fd)
-{
-    unw_cursor_t cursor; 
-    unw_context_t *uc=(unw_context_t *)(a_uc); 
-    unw_word_t ip, sp; 
-    static char symname[10000];
-	static char cxx_name[102400];
-
-	static char outbuf[102400];
-
-	for(int i = 0; i < (int) NGREG; ++i) {
-        uc_out("reg[%02d:%s]=\t0x" REGFORMAT, i, g_regs_name[i], uc->uc_mcontext.gregs[i]);
-	}
-    uc_out("Stack trace:");
-
-    int ret = unw_init_local(&cursor, uc);
-    do {
-        if (ret) break;
-
-        ret = unw_get_reg(&cursor, UNW_REG_IP, &ip);
-        ret = unw_get_reg(&cursor, UNW_REG_SP, &sp);
-        ret = unw_get_proc_name(&cursor, symname, sizeof(symname), NULL);
-        size_t name_len = sizeof(cxx_name);
-        int status = 0;
-        __cxa_demangle(symname, cxx_name, &name_len, &status);
-        char *pname = symname;
-        if (status == 0) 
-            pname = cxx_name;
-        uc_out("%s: ip: %lx, sp: %lx", pname, (long) ip, (long) sp);
-    } while (unw_step(&cursor) > 0);
-    uc_out("End of stack trace.");
-}
-*/
-
 static const char * g_fregs_name[8] =
 {
     "",
