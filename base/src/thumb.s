d72093e45e31053d425d3470e10717996b632f92
piboye <piboye@qq.com>
2015-01-14 21:31:28 +0800

    ok

--- base/src/thumb.s
+++ base/src/thumb.s
@@ -3,9 +3,7 @@
 .type jump_to_real_func, @function
 .align 16
 jump_to_real_func:
-    #pushq  -0x27(%rip) # this instruction has 7 bytes 
-    #popq   %rbx
-    leaq -0x27(%rip), %rbx
+    leaq -0x27(%rip), %rbx # this instruction has 7 bytes 
     movq (%rbx), %r12  #function address
     movq 0x8(%rbx), %r13 # this point
     movq 0x10(%rbx), %r14 # method function address
