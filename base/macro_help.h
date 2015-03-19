cbf7a4c67e62f2cb305099da28220a3182556098
piboye <piboye@qq.com>
2015-03-19 18:38:20 +0800

    add macro help

---
+++ base/macro_help.h
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  macro_help.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/19/2015 03:11:40 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  piboye
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef __CONET_MACRO_HELP_H__
+#define __CONET_MACRO_HELP_H__
+
+#ifndef BOOST_PP_VARIADICS 
+//  gcc 必须定义这个， 不然会定义为 0, 导致 BOOST_PP_VARIADIC_TO_SEQ 用不了
+# define BOOST_PP_VARIADICS 1
+#endif 
+
+#include "boost/preprocessor.hpp"
+
+#define CONET_ARG_DEF(z, n, t) \
+            BOOST_PP_COMMA_IF(n) \
+                BOOST_PP_CAT(arg_type_,n) BOOST_PP_CAT(arg, n)
+
+#endif /* end of include guard */
+
